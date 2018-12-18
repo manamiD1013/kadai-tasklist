a0dfc215132859612d4fd8f72ed1ef4209b040ce
manami.doi <m0113317d2@edu.teu.ac.jp>
2018-12-18 17:48:44 +0000

    top page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -7,7 +7,9 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class="navbar-item">{!! link_to_route('tasks.create', '新規作成', [], ['class' => 'nav-link']) !!}</li>
+                <!--<li class="navbar-item">{!! link_to_route('tasks.create', '新規作成', [], ['class' => 'nav-link']) !!}</li>-->
+                <li class="navbar-item"><a href="#" class="nav-link">Signup</a></li>
+                <li class="navbar-item"><a href="#" class="nav-link">Logout</a></li>
             </ul>
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,6 @@
 
 Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
+Route::get('/', function () {
+    return view('welcome');
+});
